6f6862d45c28cf3d097c45310ac6ff9e0c96e557
andreafalzone91-lang <andreafalzone91@live.it>
2026-07-11 23:36:47 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'reparto-live-v6';
+const CACHE = 'reparto-live-v7';
 const FILES = ['./', './index.html', './styles.css', './auth.css', './app.js', './manifest.json', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
