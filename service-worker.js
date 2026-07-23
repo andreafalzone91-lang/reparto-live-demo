cc1154288850d01afa5ffb3c8423037eb1951b77
andreafalzone91-lang <andreafalzone91@live.it>
2026-07-23 16:38:59 +0200

    Versione 72: quantità automatica vasca raclage

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'reparto-live-v71';
+const CACHE = 'reparto-live-v72';
 const FILES = ['./', './index.html', './styles.css', './auth.css', './supabase.js', './app.js', './manifest.json', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(Promise.all([caches.open(CACHE).then(cache => cache.addAll(FILES)),self.skipWaiting()])));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(()=>self.clients.claim())));
